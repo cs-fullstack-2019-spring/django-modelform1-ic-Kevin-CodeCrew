7a8544153ce5734b6b5810e90dbc751705980f7c
Kevin Yancy <kevin@code-crew.org>
2019-02-27 15:26:56 -0600

    inti

--- m_f_proj/m_f_app/views.py
+++ m_f_proj/m_f_app/views.py
@@ -11,9 +11,15 @@ def index(request):
 
 # This page will provide a form to add users
 def users(request):
-    user_form = NewUserForm()
+    new_form = NewUserForm()
 
     if request.method == "POST":
         print('Got posted data')
+        new_form = NewUserForm(request.POST)
+        if new_form.is_valid():
+            new_form.save(commit=True)
+            return index(request) # Go back to home
+        else:
+            print('Error Not Valid')
 
-    return render(request, 'm_f_app/users.html', {'userform': user_form})
+    return render(request, 'm_f_app/users.html', {'userform': new_form})
