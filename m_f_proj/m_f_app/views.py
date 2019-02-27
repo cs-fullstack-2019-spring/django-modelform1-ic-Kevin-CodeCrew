9191f426c334315d62c08cb54146023a511d29d1
Kevin Yancy <kevin@code-crew.org>
2019-02-27 14:46:33 -0600

    inti

--- m_f_proj/m_f_app/views.py
+++ m_f_proj/m_f_app/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # This view handles the landing page
+from .forms import NewUserForm
+
+
 def index(request):
     return render(request, 'm_f_app/index.html')
 
 
 # This page will provide a form to add users
 def users(request):
-    return render(request, 'm_f_app/users.html')
+    user_form = NewUserForm()
+
+    if request.method == "POST":
+        print('Got posted data')
+
+    return render(request, 'm_f_app/users.html', {'userform': user_form})
